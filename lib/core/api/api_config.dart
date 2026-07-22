df8d59c8210bec9791c00990e2853ec82cc3da39
van5hjha <krishj41205@gmail.com>
2026-07-22 23:12:00 +0530

    config: update api base url to production

--- lib/core/api/api_config.dart
+++ lib/core/api/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
   static const String baseUrl =
-      'http://127.0.0.1:8000';
+      'https://wondertal-backend-production.up.railway.app';
   static bool underMaintenance = true;
 }
